f8c57b42951665207ab33b0321544c41f81410c9
Evan Conway <evanconwaymusic@gmail.com>
2023-12-11 17:06:22 -0500

    add day 3 part 1 success

--- src/days/day_3.ts
+++ src/days/day_3.ts
@@ -5,25 +5,69 @@ interface Cell {
     char: string,
 }
 
-export const day2 = (inputFilePath: string) => {
+export const day3 = (inputFilePath: string) => {
     const grid: Cell[][] = [];
+    const NUMS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 
     const part1 = () => {
         const cellsVisited = new Set<number>();
 
-        const processLocation = (x: number, y: number) => {
+        const getNumberAtLoc = (y: number, x: number) => {
+            if (y < 0 || y >= grid.length) return 0;
+            const row = grid[y];
+            if (x < 0 || x >= row.length) return 0;
+            const cell = row[x];
+            if (cellsVisited.has(cell.id)) return 0;
+            if (!NUMS.includes(cell.char)) return 0;
+            let indexStart = x;
+            let indexEnd = x;
+            while (grid[y][indexStart - 1] !== undefined && NUMS.includes(grid[y][indexStart - 1].char)) indexStart--;
+            while (grid[y][indexEnd + 1] !== undefined && NUMS.includes(grid[y][indexEnd + 1].char)) indexEnd++;
+            let foundStr = "";
+            for (let i = indexStart; i <= indexEnd; i++) {
+                foundStr += grid[y][i].char;
+                cellsVisited.add(grid[y][i].id);
+            }
+            return Number.parseInt(foundStr);
+        };
+
+        const getTotalSurroundingPos = (y: number, x: number) => {
+            const cell = grid[y][x]; // 
+            let result = 0;
+            result += getNumberAtLoc(y, x + 1);
+            result += getNumberAtLoc(y, x - 1);
+            result += getNumberAtLoc(y + 1, x);
+            result += getNumberAtLoc(y - 1, x);
+            result += getNumberAtLoc(y + 1, x + 1);
+            result += getNumberAtLoc(y + 1, x - 1);
+            result += getNumberAtLoc(y - 1, x + 1);
+            result += getNumberAtLoc(y - 1, x - 1);
+            return result;
+        };
+
+        let result = 0;
+
+        const processLocation = (y: number, x: number) => {
             const cell = grid[y][x];
             if (cellsVisited.has(cell.id)) return;
-            cellsVisited.add(cell.id);
-            // do stuff
+            if (cell.char === '.') {
+                cellsVisited.add(cell.id);
+                return;
+            }
+            if (NUMS.includes(cell.char)) return; // only add numbers to cells visited when returned for part number
+            // if we're here, the cell must be an unprocessed part
+            result += getTotalSurroundingPos(y, x);
         };
 
         for (let y = 0; y < grid.length; y++) {
             const row = grid[y];
             for (let x = 0; x < row.length; x++) {
 
+                processLocation(y, x);
             }
         }
+
+        return result;
     };
 
     fs.readFile(inputFilePath, 'utf8', (err, data) => {
@@ -31,7 +75,7 @@ export const day2 = (inputFilePath: string) => {
             console.error(err);
             return;
         }
-        const lines = data.split('\r\n');
+        const lines = data.split('\n');
 
         // setup grid
         let ids = 0;
@@ -46,6 +90,6 @@ export const day2 = (inputFilePath: string) => {
             }
         }
 
-        part1();
+        console.log(part1());
     });
 };
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { day1 } from "./days/day_1";
 import { day2 } from "./days/day_2";
+import { day3 } from "./days/day_3";
 
 //day1('input/day1.txt');
-day2('input/day2.txt');
+day3('input/day3.txt');

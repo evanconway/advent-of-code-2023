40f8a8e623ac08c9962bb9d3c697b638ba7c46d7
AceOfHeart5 <evanconwaymusic@gmail.com>
2023-12-11 23:24:59 -0500

    add broken logic

--- src/days/day_4.ts
+++ src/days/day_4.ts
@@ -4,13 +4,12 @@ interface Card {
     id: number,
     cardNumbers: number[],
     yourNumbers: number[],
-    points?: number
 }
 
 export const day4 = () => {
     const lineToCard = (line: string) => {
         const idDataStr = line.split(':');
-        const id = Number.parseInt(idDataStr[0].split(' ')[1]);
+        const id = Number.parseInt(idDataStr[0].split(' ').filter(s => s !== '')[1]);
         const numbersStr = idDataStr[1].split('|');
         const card: Card = {
             id,
@@ -33,21 +32,28 @@ export const day4 = () => {
     };
 
     const part2 = (cards: Card[]) => {
-        const pointCards = cards.map(c => ({...c, points: cardToPointsWon(c) }));
-        const cardMap = new Map<number, Card>();
-        for (let i = 0; i < pointCards.length; i++) cardMap.set(cards[i].id, cards[i]);
+        interface CardWithPoints extends Card {
+            points: number
+        }
+        const pointCards: CardWithPoints[] = cards.map(c => ({...c, points: cardToPointsWon(c) }));
+        const cardMap = new Map<number, CardWithPoints>();
+        for (let i = 0; i < pointCards.length; i++) cardMap.set(pointCards[i].id, pointCards[i]);
         // reuse card input as stack
         let cardsWithNoWin = 0;
-        while (cards.length > 0) {
+        while (pointCards.length > 0) {
             const card = pointCards.pop()!;
             if (card.points === 0) cardsWithNoWin++;
             else {
-                let cardId = card?.id + 1;
+                for (let cardId = card.id + 1; cardId <= card.id + card.points; cardId++) {
+                    const cardToPush = cardMap.get(cardId);
+                    if (cardToPush !== undefined) pointCards.push(cardToPush);
+                }
             }
         }
+        return cardsWithNoWin;
     };
 
-    fs.readFile('input/day4.txt', 'utf8', (err, data) => {
+    fs.readFile('input/day4test.txt', 'utf8', (err, data) => {
         if (err) {
             console.error(err);
             return;
@@ -55,5 +61,6 @@ export const day4 = () => {
         const lines = data.split('\r\n');
         const cards = lines.map(line => lineToCard(line));
         console.log('part 1:', part1(cards));
+        console.log('part 2:', part2(cards));
     });
 };
